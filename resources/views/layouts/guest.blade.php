4cff1613a0245f9250bbdc4fa2d127696d0a044a
Dhaby <105906444+dhabyap@users.noreply.github.com>
2026-04-23 15:37:23 +0700

    feat: Extract Brutalist CSS to Global Asset (#36)
    * feat: implement brutalist landing page - closes #29
    
    * feat: extract brutalist css to global asset - closes #31

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,6 +10,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&family=Bebas+Neue&family=Instrument+Serif:ital@0;1&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
